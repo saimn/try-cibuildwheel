322fac6dad8751b11f8b21e3397a5d84d44ca5dd
Simon Conseil <contact@saimon.org>
2018-09-17 13:34:14 +0200

    Don't use numpy wheels (certificate issue)

--- build-deps.sh
+++ build-deps.sh
@@ -2,20 +2,16 @@
 
 set -e
 
-# Extra options for pip
-if [ -n "$IS_OSX" ]; then
-    suffix=scipy_installers
-else
-    suffix=manylinux
-fi
-pip_opts="--only-binary numpy --find-links https://nipy.bic.berkeley.edu/$suffix"
-
 echo "# Installing Numpy & Cython"
-echo "pip options: $pip_opts"
-pip install $pip_opts numpy==1.10.4 Cython
+pip install numpy==1.10.4 Cython
 
-echo "# Installing cfitsio"
-curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xvz
-cd cfitsio
-./configure && make shared && make install
-cd ..
+
+if [ -n "$IS_OSX" ]; then
+    brew install cfitsio pkg-config
+else
+    echo "# Installing cfitsio"
+    curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xvz
+    cd cfitsio
+    ./configure && make shared && make install
+    cd ..
+fi
