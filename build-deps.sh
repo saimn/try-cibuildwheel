a73d81b431c44f390f4559dd411114d7e10a4bf6
Simon Conseil <contact@saimon.org>
2018-09-17 13:04:46 +0200

    Copy build script in sources dir

--- build-deps.sh
+++ build-deps.sh
@@ -2,13 +2,8 @@
 
 set -e
 
-echo "# Installing Numpy & Cython"
-pip install numpy==1.10.4 Cython
-
 echo "# Installing cfitsio"
 curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xvz
 cd cfitsio
-./configure
-make shared
-make install
+./configure && make shared && make install
 cd ..
