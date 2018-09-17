c67750ca2ad29667bcfb77efa8b88173e56f311d
Simon Conseil <contact@saimon.org>
2018-09-17 13:18:13 +0200

    debug

--- build-deps.sh
+++ build-deps.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+# Extra options for pip
+if [ -n "$IS_OSX" ]; then
+    local suffix=scipy_installers
+else
+    local suffix=manylinux
+fi
+local pip_opts="--only-binary numpy --find-links https://nipy.bic.berkeley.edu/$suffix"
+
+echo "# Installing Numpy & Cython"
+echo "pip options: $pip_opts"
+pip install $pip_opts numpy==1.10.4 Cython
+
 echo "# Installing cfitsio"
 curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xvz
 cd cfitsio
