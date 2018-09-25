8ea1bd31b8611ded970d2fb637a8fccc6d12ed6d
Simon Conseil <contact@saimon.org>
2018-09-25 12:48:07 +0200

    Try again

--- build-deps.sh
+++ build-deps.sh
@@ -5,15 +5,8 @@ set -e
 echo "# Installing Numpy"
 pip install numpy==1.10.4
 
-
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew install cfitsio
-    brew info cfitsio
-    echo $(brew --prefix cfitsio)
-else
-    echo "# Installing cfitsio"
-    curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xvz
-    cd cfitsio
-    ./configure && make shared && make install
-    cd ..
-fi
+echo "# Installing cfitsio"
+curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xvz
+cd cfitsio
+./configure && make shared && make install
+cd ..
