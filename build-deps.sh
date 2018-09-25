72629302fa0681a8985ae2b02951fab832fdde00
Simon Conseil <contact@saimon.org>
2018-09-25 13:03:05 +0200

    Switch back to use brew

--- build-deps.sh
+++ build-deps.sh
@@ -5,8 +5,15 @@ set -e
 echo "# Installing Numpy"
 pip install numpy==1.10.4
 
-echo "# Installing cfitsio"
-curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xvz
-cd cfitsio
-./configure && make shared && make install
-cd ..
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew install cfitsio
+    brew info cfitsio
+    echo $(brew --prefix cfitsio)
+else
+    echo "# Installing cfitsio"
+    curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xvz
+    cd cfitsio
+    ./configure && make shared && make install
+    cd ..
+fi
