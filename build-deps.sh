a279c8825d27337666494ecfc7d02a5ab2de901b
Simon Conseil <contact@saimon.org>
2018-09-25 09:29:15 +0200

    Guessing brew path

--- build-deps.sh
+++ build-deps.sh
@@ -7,7 +7,9 @@ pip install numpy==1.10.4 Cython
 
 
 if [ -n "$IS_OSX" ]; then
-    brew install cfitsio pkg-config
+    brew install cfitsio
+    brew info cfitsio
+    echo $(brew --prefix cfitsio)
 else
     echo "# Installing cfitsio"
     curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xvz
