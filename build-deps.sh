43776211f4200d00fcdf6bd280a9462612a2e992
Simon Conseil <contact@saimon.org>
2018-10-01 00:39:26 +0200

    Use different Numpy versions

--- build-deps.sh
+++ build-deps.sh
@@ -3,8 +3,18 @@
 set -e
 
 echo "# Installing Numpy"
-pip install numpy==1.10.4
-
+pyversion=$(python -c 'import sys; print("%d.%d" % (sys.version_info.major, sys.version_info.minor))')
+case "$pyversion" in 
+  3.5)
+    pip install numpy==1.10.4
+    ;;
+  3.6)
+    pip install numpy==1.11.3
+    ;;
+  3.7)
+    pip install numpy==1.14.5
+    ;;
+esac
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew install cfitsio
