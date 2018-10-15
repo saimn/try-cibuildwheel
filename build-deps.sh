e262684694c3dbed8f68665305ee84048a82b8b0
Simon Conseil <contact@saimon.org>
2018-10-15 23:21:56 +0200

    test astropy

--- build-deps.sh
+++ build-deps.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "# Installing Numpy"
 pyversion=$(python -c 'import sys; print("%d.%d" % (sys.version_info.major, sys.version_info.minor))')
-case "$pyversion" in 
+case "$pyversion" in
   3.5)
     pip install numpy==1.10.4
     ;;
@@ -15,16 +15,3 @@ case "$pyversion" in
     pip install numpy==1.14.5
     ;;
 esac
-
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew install cfitsio
-    brew info cfitsio
-    echo $(brew --prefix cfitsio)
-else
-    echo "# Downloading cfitsio"
-    curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xz
-    echo "# Installing cfitsio"
-    cd cfitsio
-    ./configure && make clean shared install
-    cd ..
-fi
